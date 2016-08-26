39d8b31984eedf7ccede0ba6c8024d8db2e075d4
Alexey Korol <korol@devexperts.com>
2016-08-26 14:01:13 +0300

    input component

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,44 +1,123 @@
 import React from 'react';
 import {themr} from 'react-css-themr';
+import classnames from 'classnames';
 
 export const INPUT = Symbol('Input');
-export const INPUT_THEME_SHAPE_OBJECT = {
-	container: React.PropTypes.string
-};
-
-import css from './Input.styl';
+export const INPUT_THEME_SHAPE_OBJECT = ({
+	container: React.PropTypes.string,
+	container_isFocued: React.PropTypes.string,
+	container_isDisabled: React.PropTypes.string,
+	container_isReadonly: React.PropTypes.string
+});
 
 export const PROP_TYPES = {
+	id: React.PropTypes.string,
 	value: React.PropTypes.oneOfType([
 		React.PropTypes.string,
 		React.PropTypes.number
 	]),
+	pattern(props, propName, componentName) {
+		const {pattern} = props;
+		const className = pattern.constructor && pattern.constructor.name;
+		if (pattern && !(pattern instanceof RegExp)) {
+			throw new Error(`Failed prop type: Invalid prop \`${propName}\` of type \`${className}\`
+			 supplied to \`${componentName}\`, expected instance of \`RegExp\`.`);
+		}
+	},
 	theme: React.PropTypes.shape(INPUT_THEME_SHAPE_OBJECT),
 	isReadonly: React.PropTypes.bool,
 	isDisabled: React.PropTypes.bool,
 	placeholder: React.PropTypes.string,
+	onWheel: React.PropTypes.func,
+	onFocus: React.PropTypes.func,
+	onBlur: React.PropTypes.func,
+	onKeyDown: React.PropTypes.func,
 	onChange: React.PropTypes.func
 };
 
-@themr(INPUT, css)
+@themr(INPUT)
 export default class Input extends React.Component {
 
 	static propTypes = PROP_TYPES;
 
-	static defaultProps = {
-		onChange() {}
-	};
+	constructor(...args) {
+		super(...args);
+		const {value} = this.props;
+
+		this.state = {
+			value,
+			isFocused: false
+		};
+	}
+
+	onChange = e => {
+		const {value} = this.state;
+		const {value: newValue} = e.target;
+		const {onChange, pattern} = this.props;
+		if ((pattern && pattern.test(newValue)) && newValue !== value) {
+			this.setState({
+				value: newValue
+			});
+			onChange && onChange(newValue);
+		}
+	}
+
+	componentWillReceiveProps(newProps) {
+		const {value} = this.state;
+		const {value: newValue} = newProps;
+		if (newValue !== value) {
+			this.setState({
+				value: newValue
+			});
+		}
+	}
 
 	render() {
-		const {theme, isDisabled, value, isReadonly, placeholder, onChange} = this.props;
+		const {theme, isDisabled, isReadonly, placeholder, onWheel, onKeyDown} = this.props;
+		const {value, isFocused} = this.state;
+
+		const className = classnames(
+			theme.container,
+			{
+				[theme.container_isReadonly]: isReadonly,
+				[theme.container_isDisabled]: isDisabled,
+				[theme.container_isFocued]: isFocused
+			}
+		);
+
+		const inputProps = {
+			disabled: isDisabled,
+			readOnly: isReadonly,
+			value,
+			onWheel,
+			placeholder,
+			className,
+			onKeyDown,
+			onFocus: this.onFocus,
+			onBlur: this.onBlur,
+			onChange: this.onChange
+		};
 
 		return (
-			<input disabled={isDisabled}
-				   readOnly={isReadonly}
-				   value={value}
-				   placeholder={placeholder}
-				   className={theme.container}
-				   onChange={onChange} />
+			<input {...inputProps}	/>
 		);
+	}
+
+	onFocus = e => {
+		const {onFocus} = this.props;
+		this.setState({
+			isFocused: true
+		});
+
+		onFocus && onFocus();
+	}
+
+	onBlur = e => {
+		const {onBlur} = this.props;
+		this.setState({
+			isFocused: false
+		});
+
+		onBlur && onBlur();
 	}
 }
--- src/components/Input/Input.page.jsx
+++ src/components/Input/Input.page.jsx
@@ -4,14 +4,27 @@ import Demo from '../../demo/Demo.jsx';
 import Input from './Input.jsx';
 import {storiesOf} from '@kadira/storybook';
 
+import css from './Input.page.styl';
+
+const darkDemoTheme = {
+	container: css.container
+};
+
 storiesOf('Input', module)
 	.add('Default', () => (
-		<Demo>
+		<Demo theme={darkDemoTheme}>
 			<Input value="Test Value"/>
 		</Demo>
-	)).add('Disabled', () => {
+	))
+	.add('Readonly', () => {
 		return (
-			<Demo>
+			<Demo theme={darkDemoTheme}>
+				<Input value="Test Value" isReadonly={true} />
+			</Demo>
+		);
+	}).add('Disabled', () => {
+		return (
+			<Demo theme={darkDemoTheme}>
 				<Input value="Test Value" isDisabled={true} />
 			</Demo>
 		);
