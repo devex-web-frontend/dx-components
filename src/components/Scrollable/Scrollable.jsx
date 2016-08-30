3a9fdacecfa91677b26047867f217d4cab62352c
Kirill Agalakov <agalakov@devexperts.com>
2016-08-30 15:08:16 +0300

    scrolling sync

--- src/components/Scrollable/Scrollable.jsx
+++ src/components/Scrollable/Scrollable.jsx
@@ -4,20 +4,56 @@ import {Scrollable as ScrollableControl} from 'scrollable/src/Scrollable';
 
 export default class Scrollable extends React.Component {
 	static propTypes = {
-		children: React.PropTypes.element
+		children: React.PropTypes.element,
+		onScroll: React.PropTypes.func,
+		scrollTop: React.PropTypes.number,
+		scrollLeft: React.PropTypes.number
 	}
 
 	_scrollable;
+	_target;
 
 	componentDidMount() {
 		this._scrollable = new ScrollableControl(ReactDOM.findDOMNode(this));
+		this._target = this._scrollable.result.detail.eventTarget;
+		if (this.props.onScroll) {
+			this._target.addEventListener('scroll', this.onScroll);
+		}
+		this.updateScroll();
+	}
+
+	componentWillUpdate(nextProps) {
+		if (nextProps.onScroll && !this.props.onScroll) {
+			this._target.addEventListener('scroll', this.onScroll);
+		} else if (!nextProps.onScroll && this.props.onScroll) {
+			this._target.removeEventListener('scroll', this.onScroll);
+		}
+	}
+
+	componentDidUpdate(prevProps) {
+		this.updateScroll();
 	}
 
 	componentWillUnmount() {
+		this._target.removeEventListener('scroll', this.onScroll);
 		this._scrollable.close();
 	}
 
 	render() {
 		return React.Children.only(this.props.children);
+	}
+
+	updateScroll() {
+		if (typeof this.props.scrollLeft !== 'undefined') {
+			this._target.scrollLeft = this.props.scrollLeft;
+		}
+		if (typeof this.props.scrollTop !== 'undefined') {
+			this._target.scrollTop = this.props.scrollTop;
+		}
+	}
+
+	onScroll = e => {
+		const {scrollLeft, scrollTop} = e.target;
+		this.props.onScroll && this.props.onScroll(scrollLeft, scrollTop);
 	}
 }
--- src/components/Scrollable/Scrollable.page.jsx
+++ src/components/Scrollable/Scrollable.page.jsx
@@ -4,41 +4,90 @@ import Scrollable from './Scrollable';
 import Demo from '../../demo/Demo';
 import css from './Scrollable.page.styl';
 import {PURE} from 'dx-util/src/react/pure';
+import Pure from '../Pure/Pure';
+import Perf from 'react-addons-perf';
+
+const Heavy = () => (
+	<div className={css.origin}>
+		<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
+		<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
+		<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
+		<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
+		<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
+		<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
+		<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
+		<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
+		<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
+		<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
+		<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
+		<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
+		<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
+		<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
+		<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
+		<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
+		<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
+		<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
+		<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
+		<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
+		<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
+		<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
+		<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
+	</div>
+);
 
 @PURE
 class ScrollablePage extends React.Component {
+	state = {}
+
 	render() {
 		return (
 			<Demo>
-				<Scrollable>
-					<div className={css.origin}>
-						<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
-						<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
-						<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
-						<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
-						<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
-						<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
-						<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
-						<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
-						<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
-						<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
-						<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
-						<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
-						<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
-						<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
-						<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
-						<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
-						<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
-						<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
-						<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
-						<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
-						<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
-						<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
-						<p>1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0</p>
-					</div>
-				</Scrollable>
+				<Pure>
+					{() => (
+						<section>
+							<h1>Origin</h1>
+							<Scrollable onScroll={this.onScroll}>
+								<Heavy/>
+							</Scrollable>
+						</section>
+					)}
+				</Pure>
+
+				<Pure check={this.state.scrollTop}>
+					{() => (
+						<section>
+							<h1>VSync</h1>
+							<Scrollable scrollTop={this.state.scrollTop}>
+								<Pure>{() => <Heavy/>}</Pure>
+							</Scrollable>
+						</section>
+					)}
+				</Pure>
+				<Pure check={this.state.scrollLeft}>
+					{() => (
+						<section>
+							<h1>HSync</h1>
+							<Scrollable scrollLeft={this.state.scrollLeft}>
+								<Pure>{() => <Heavy/>}</Pure>
+							</Scrollable>
+						</section>
+					)}
+				</Pure>
+				<section>
+					<h1>VHSync</h1>
+					<Scrollable scrollTop={this.state.scrollTop} scrollLeft={this.state.scrollLeft}>
+						<Pure>{() => <Heavy/>}</Pure>
+					</Scrollable>
+				</section>
 			</Demo>
 		);
+	}
+
+	onScroll = (scrollLeft, scrollTop) => {
+		this.setState({
+			scrollLeft,
+			scrollTop
+		});
 	}
 }
 
