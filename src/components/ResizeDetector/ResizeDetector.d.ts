c74bc54bc603d7b0dcc035fc29b228dd8110442e
Kirill Agalakov <agalakov@devexperts.com>
2017-02-21 14:33:20 +0300

    missing typings

--- src/components/ResizeDetector/ResizeDetector.d.ts
+++ src/components/ResizeDetector/ResizeDetector.d.ts
@@ -2,7 +2,7 @@ declare type TResizeDetectorProps = {
 	theme?: {
 		container?: string
 	},
-	onResize: (event: UIEvent) => any;
+	onResize?: (event: UIEvent) => any;
 };
 declare const ResizeDetector: React.ComponentClass<TResizeDetectorProps>;
 export default ResizeDetector;
