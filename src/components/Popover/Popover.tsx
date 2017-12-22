e4b0b83be5230b42c9996369ba8f232df2e2143f
Anton Koshkin <a.v.koshkin.91@gmail.com>
2017-12-22 16:51:45 +0300

    bugfix/popver-close-on-scroll | fixes

--- src/components/Popover/Popover.tsx
+++ src/components/Popover/Popover.tsx
@@ -8,7 +8,6 @@ import { PURE } from 'dx-util/lib/react/pure';
 import { BoundsUpdateDetector } from '../BoundsUpdateDetector/BoundsUpdateDetector';
 import { THROTTLE } from 'dx-util/lib/function/throttle';
 
-import TPortalProps = ReactOverlays.Portal.TPortalProps;
 import { withTheme } from '../../util/react/withTheme';
 import { ComponentClass, MouseEventHandler, ReactNode } from 'react';
 import { ObjectClean } from 'typelevel-ts';
@@ -48,7 +47,7 @@ export type TFullPopoverProps = {
 	onMouseDown?: MouseEventHandler<Element>,
 	placement: PopoverPlacement,
 	align: PopoverAlign,
-	container?: TPortalProps['container'],
+	container?: Element | null,
 	onRequestClose?: () => any,
 	hasArrow?: boolean,
 	theme: {
@@ -171,17 +170,15 @@ class RawPopover extends React.Component<TFullPopoverProps, TPopoverState> {
 			</BoundsUpdateDetector>
 		);
 		if (closeOnClickAway) {
-			child = (
-				<RootClose onRootClose={onRequestClose}>
-					{child}
-				</RootClose>
-			);
+			child = <RootClose onRootClose={onRequestClose}>{child}</RootClose>;
+		}
+
+		if (container) {
+			child = <EventListener target={container} onScroll={this.onScroll}>{child}</EventListener>;
 		}
 
 		return (
-			<EventListener onResize={this.onResize}
-			               onScroll={this.onScroll}
-			               target="window">
+			<EventListener onResize={this.onResize} target="window">
 				<Portal container={container}>
 					{child}
 				</Portal>
